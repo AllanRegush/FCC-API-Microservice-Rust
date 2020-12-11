36280cd6a246c0dedcb2b552d8c4f0776de5dbd8
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-12-11 00:58:54 -0600

    timestamp route

--- timestamp-microservice/src/main.rs
+++ timestamp-microservice/src/main.rs
@@ -1,8 +1,26 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
+}
+
+#[derive(Deserialize, Serialize)]
+struct Time {
+    unix: i64,
+    utc: String
+}
+
+#[get("/timestamp")]
+async fn timestamp() -> impl Responder {
+    let now = Utc::now();
+    let time = Time {
+        unix: now.timestamp(),
+        utc: now.to_rfc2822()
+    };
+    HttpResponse::Ok().json(time)
 }
 
 #[actix_web::main]
@@ -10,6 +28,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new( || {
         App::new()
         .service(hello)
+        .service(timestamp)
     })
     .bind("127.0.0.1:8080")?
     .run()

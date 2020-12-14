4c0a7cd935915cda8985d9faf65f08466973cc2e
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-12-13 19:51:25 -0600

    added date path route

--- timestamp-microservice/src/main.rs
+++ timestamp-microservice/src/main.rs
@@ -1,6 +1,6 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
 use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc};
+use chrono::{Utc, TimeZone, LocalResult};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -13,7 +13,7 @@ struct Time {
     utc: String
 }
 
-#[get("/timestamp")]
+#[get("api/timestamp/")]
 async fn timestamp() -> impl Responder {
     let now = Utc::now();
     let time = Time {
@@ -23,12 +23,34 @@ async fn timestamp() -> impl Responder {
     HttpResponse::Ok().json(time)
 }
 
+
+#[get("api/timestamp/{date}")]
+async fn timestamp_date(date: web::Path<i64>) -> impl Responder {
+    
+    let now = Utc.timestamp_millis_opt(*date);
+    match now {
+        LocalResult::Single(dt) => {
+            let time = Time {
+                unix: dt.timestamp(),
+                utc: dt.to_rfc2822()
+            };
+            HttpResponse::Ok().json(time)
+        }
+        _ => HttpResponse::Ok().content_type("application/json").body("{ \"error\":\"Invaild Date\" }")
+    }
+    /*
+    let now = Utc::now();
+    */
+
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new( || {
         App::new()
         .service(hello)
         .service(timestamp)
+        .service(timestamp_date)
     })
     .bind("127.0.0.1:8080")?
     .run()
